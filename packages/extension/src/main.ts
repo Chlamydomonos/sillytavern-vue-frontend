f82fae8066910a3d7ad605624a2f155cbf223e5c
Chlamydomonos <2457242458@qq.com>
2025-01-17 21:06:14 +0800

    fix

--- packages/extension/src/main.ts
+++ packages/extension/src/main.ts
@@ -7,6 +7,6 @@ import App from './App.vue';
 const tavernContext = getContext();
 
 const extensionsSettingsElem = document.getElementById('#extensions_settings');
-const appElem = extensionsSettingsElem!.appendChild(new HTMLDivElement());
+const appElem = extensionsSettingsElem!.appendChild(document.createElement('div'));
 
 createApp(App).mount(appElem);
