78165efe65582cdcd8ec8ce38b6d7e741d67df6f
Chlamydomonos <2457242458@qq.com>
2025-04-18 17:56:17 +0800

    1.3.5

--- packages/extension/src/tavern/wrapper-api/get-raw-chat.ts
+++ packages/extension/src/tavern/wrapper-api/get-raw-chat.ts
@@ -1,8 +1,15 @@
 import { getContext } from 'sillytavern-extension-api';
 
-export const getRawChat = () =>
-    getContext().chat.map((c) => {
-        const role = c.is_user ? 'user' : 'assistant';
-        const content = c.swipes ? c.swipes[c.swipe_id] : c.mes;
-        return { role, content };
-    });
+export const getRawChat = () => {
+    const chat = getContext().chat;
+    const rawChat: { role: string; content: string }[] = [];
+    for (const item of chat) {
+        const role = item.is_user ? 'user' : 'assistant';
+        if (item.swipes && item.swipe_id > item.swipes.length) {
+            continue;
+        }
+        const content = item.swipes ? item.swipes[item.swipe_id] : item.mes;
+        rawChat.push({ role, content });
+    }
+    return rawChat;
+};
